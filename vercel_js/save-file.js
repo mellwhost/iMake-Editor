279cfd2d2a4b1bcfef3e483949c6b21b2e2ad676
Mellw313 <ygormellw266@gmail.com>
2026-02-26 11:54:59 -0300

    Delete vercel_js directory

--- vercel_js/save-file.js
+++
@@ -1,9 +0,0 @@
-function save_file() {
-  const codigo = document.getElementById("editor-code").value;
-  const blob = new Blob([codigo], { type: "text/javascript" });
-  const link = document.createElement("a");
-  link.href = URL.createObjectURL(blob);
-  link.download = "script.js";
-  link.click();
-  URL.revokeObjectURL(link.href);
-}   
